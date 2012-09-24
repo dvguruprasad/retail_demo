cea574b1dd46ab5859912d88b4523f3a7f0666be
Guruprasad <dv.guruprasad@gmail.com>
2012-09-24 18:22:02 +0530

    spawned a spree app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RetailDemo::Application.config.secret_token = '1577bfcf49ecc4a54e63ea63976461c801a2c3c6d4bad03e282664d59b4b247f4af774c0c0c9b8043b374399d9026dfdb672ec691a90fc98e6cc9c6fabe36143'

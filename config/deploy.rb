48bb0357c2634eb8e7ab57a1ddbf4d3b8fa0cddd
Guruprasad <dv.guruprasad@gmail.com>
2012-11-06 10:50:17 +0530

    using capistrano-ext to deploy to different environments.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,23 +1,21 @@
-set :application, "Retail Demo"
+require 'rvm/capistrano'
+require 'bundler/capistrano'
+require 'capistrano/ext/multistage'
+
+# Add RVM's lib directory to the load path.
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+
+# Load RVM's capistrano plugin.
+require "rvm/capistrano"
+
+set :stages, %w(testing production)
+set :default_stage, "testing"
+
+set :application, "retail_demo"
 set :repository,  "git@github.com:dvguruprasad/retail_demo.git"
 
 set :scm, :git
-set :deploy_to, "/var/www"
+set :deploy_to, "/var/www/#{application}"
 set :deploy_via, :remote_cache
-
-server "spree-demo.corporate.thoughtworks.com", :app, :web, :db, :primary => true
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+set :branch, "wip"
+default_run_options[:pty] = true

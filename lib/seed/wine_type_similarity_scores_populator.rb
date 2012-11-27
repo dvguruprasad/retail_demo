2fa676e1a90c0c8e10ea7c93340fb2d2534e3fac
Pramod N <npramod05@gmail.com>
2012-11-27 15:39:19 +0530

    changing the seed scripts to add recommendation type for wine taxon

--- lib/seed/wine_type_similarity_scores_populator.rb
+++ lib/seed/wine_type_similarity_scores_populator.rb
@@ -4,6 +4,14 @@ class WineTypeSimilarityScoresPopulator
   end
 
   def populate
+    wine_taxon = Spree::Taxon.find_by_name('Wines')
+    if wine_taxon.nil?
+      raise "No taxon named Wines!! Seed abort!!"
+    end 
+    wine_config = TaxonConfiguration.find_by_taxon_id_and_recommendation_type(wine_taxon.id, 'rec')
+    if wine_config.nil?
+      TaxonConfiguration.create(:taxon_id =>wine_taxon.id, :recommendation_type => 'rec')
+    end
     lines = IO.read(@similarity_scores_csv_file, :encoding => "UTF-8").split("\n")
     lines.each do |line|
       data = line.split(",")

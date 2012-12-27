e13ef11dc61915e30c1c6d8fe92d7bd46b5e9a97
rohit kumar <roh422@yahoo.com>
2012-12-27 17:43:12 +0530

    Galeel/Rohit - changes to the UI, customized header and footer

---
+++ app/overrides/all.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(:virtual_path => "spree/layouts/admin",
+                     :name => "footer-content",
+                     :replace => "div#footer p",
+                     :partial => "layouts/footer_content")
+
+
--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -8,7 +8,9 @@
 Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
-  # config.site_name = "Spree Demo Site"
+  #config.site_name = "Spree Demo Site"
+  config.logo = "admin/bg/thrift.png"
+  config.admin_interface_logo = "admin/bg/thrift.png"
 end
 
 Spree.user_class = "Spree::User"
